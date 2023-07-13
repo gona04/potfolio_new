d27a8590f0deb4516c7ebaeb5b5a5bec2bc526ac
Goonja <gandhi.goonja@gmail.com>
2023-07-13 17:42:09 -0400

    made changes for deployment to work smoothly
    changed angular.json build
    removed ../../ from the paths
    and added assests/icons etc in build of angular.json
    then created a docs folder for running the code

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -27,13 +27,13 @@ export class FooterComponent implements OnInit {
 
         if(newEvent.url == "/it_work") {
           this.displayloop = [];
-          this.displayloop.push({alt_text: "linkedin", icon_address: "../../assets/icons/linkedin.svg", workLink: "https://in.linkedin.com/in/goonja-gandhi-66a64526", class_name: "linkedin-styling"})
-          this.displayloop.push({alt_text: "github_link", icon_address: "../../assets/icons/github.svg", workLink:"https://github.com/gona04", class_name: "github-styling"})
+          this.displayloop.push({alt_text: "linkedin", icon_address: "assets/icons/linkedin.svg", workLink: "https://in.linkedin.com/in/goonja-gandhi-66a64526", class_name: "linkedin-styling"})
+          this.displayloop.push({alt_text: "github_link", icon_address: "assets/icons/github.svg", workLink:"https://github.com/gona04", class_name: "github-styling"})
         } else if( newEvent.url == "/music_work" ) {
           this.displayloop = [];
-          this.displayloop.push({alt_text:"IDBM link", icon_address: "../../assets/icons/imdb.svg", workLink:"https://www.imdb.com/name/nm9123904/", class_name:"imdb-styling"})
-          this.displayloop.push({alt_text: "Sound Cloud", icon_address: "../../assets/icons/soundcloud.svg", workLink:"https://soundcloud.com/goonja-gandhi", class_name:"soundcloud-styling"})
-          this.displayloop.push({alt_text: "Youtube Link", icon_address: "../../assets/icons/youtube-gray.svg", workLink: "https://www.youtube.com/@goonjagandhi4090", class_name: "youtube-styling"})
+          this.displayloop.push({alt_text:"IDBM link", icon_address: "assets/icons/imdb.svg", workLink:"https://www.imdb.com/name/nm9123904/", class_name:"imdb-styling"})
+          this.displayloop.push({alt_text: "Sound Cloud", icon_address: "assets/icons/soundcloud.svg", workLink:"https://soundcloud.com/goonja-gandhi", class_name:"soundcloud-styling"})
+          this.displayloop.push({alt_text: "Youtube Link", icon_address: "assets/icons/youtube-gray.svg", workLink: "https://www.youtube.com/@goonjagandhi4090", class_name: "youtube-styling"})
         }
       });
   }
